21913af47cdcbfe476a303828bbb67694224dda3
cedric simon <simonced@gmail.com>
2017-08-13 18:57:52 +0900

    add sii-mode.el

--- emacs/sii-mode.el
+++ emacs/sii-mode.el
@@ -1,11 +1,24 @@
 ;; a simple major mode, sii-mode
 ;; still has issues with comments containing "strings"
 
+
+(defvar sii-mode-syntax-table nil "Syntax table for `sii-mode'.")
+
+
+(setq sii-mode-syntax-table
+      (let ( (synTable (make-syntax-table)))
+        ;; python style comment: “# …”
+        (modify-syntax-entry ?# "<" synTable)
+        (modify-syntax-entry ?\n ">" synTable)
+        synTable))
+
+
 (setq sii-highlights
-      '(("#.*" . font-lock-comment-face)
-		(".*?:" . font-lock-function-name-face)))
+      '((".*?:" . font-lock-variable-name-face)))
 
 
 (define-derived-mode sii-mode fundamental-mode "sii"
   "major mode for editing mymath language code."
-  (setq font-lock-defaults '(sii-highlights)))
+  (setq font-lock-defaults '(sii-highlights))
+  (set-syntax-table sii-mode-syntax-table)
+  )
